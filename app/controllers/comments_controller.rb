89d294d3c6b9a5ef9d240926aeb9e98e370958a3
daqing <daqing1986@gmail.com>
2011-04-18 21:09:38 +0800

    make ajax request to add issue comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,14 @@ class CommentsController < ApplicationController
     @comment = @issue.comments.new(params[:comment])
     @comment.user = current_user
 
-    if @comment.save
-      redirect_to @issue
-    else
-      # TODO: create comment failed !
-      redirect_to root_path
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @issue }
+        format.js
+      else
+        format.html { redirect_to root_path }
+        format.js { render :nothing => true }
+      end
     end
   end
 

5184b1e8ee7edb5f546e82aba4592d46b8d39009
daqing <daqing1986@gmail.com>
2011-04-18 19:07:22 +0800

    make Ajax request to add todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,11 +4,14 @@ class TodoItemsController < ApplicationController
     @project = @issue.project
     @todo_item = @issue.todo_items.new(params[:todo_item])
 
-    if @todo_item.save
-      redirect_to [@project, @issue]
-    else
-      # TODO: Error!
-      redirect_to root_path
+    respond_to do |format|
+      if @todo_item.save
+        format.html { redirect_to [@project, @issue] }
+        format.js
+      else
+        format.html { redirect_to [@project, @issue] }
+        format.js { render :nothing => true }
+      end
     end
   end
 

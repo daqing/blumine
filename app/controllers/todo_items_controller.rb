f1ca97b12d38002e6231cd23d211a9eaf9d30915
daqing <daqing1986@gmail.com>
2011-04-19 00:07:17 +0800

    mark todo as done, new and delete todo item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
+  before_filter :find_todo_item, :except => :create
+
   def create
     @issue = Issue.find(params[:issue_id])
     @project = @issue.project
@@ -16,20 +18,40 @@ class TodoItemsController < ApplicationController
   end
 
   def do_it
-    @todo_item = TodoItem.find(params[:id])
-    @todo_item.do_it!
-
-    render :nothing => true
+    workflow_action "do_it"
   end
 
   def undo
-    @todo_item = TodoItem.find(params[:id])
-    @todo_item.undo!
-
-    render :nothing => true
+    workflow_action "undo"
   end
 
   def destroy
+    respond_to do |format|
+      if @todo_item.destroy
+        format.html { redirect_to [@todo_item.issue.project, @todo_item.issue] }
+        format.js { render :nothing => true }
+      else
+        format.html { redirect_to root_path }
+        format.js { render :text => "Error", :status => 500 }
+      end
+    end
   end
 
+  private
+    def find_todo_item
+      @todo_item = TodoItem.find(params[:id])
+    end
+
+    def workflow_action(action)
+      respond_to do |format|
+        begin
+          @todo_item.send(action + '!')
+          format.html { redirect_to [@todo_item.issue.project, @todo_item.issue] }
+          format.js { render :nothing => true }
+        rescue Workflow::NoTransitionAllowed => e
+          format.html { redirect_to root_path }
+          format.js { render :text => e.message, :status => 500 }
+        end
+      end
+    end
 end
--- test/functional/todo_items_controller_test.rb
+++ test/functional/todo_items_controller_test.rb
@@ -3,11 +3,12 @@ require 'test_helper'
 class TodoItemsControllerTest < ActionController::TestCase
   setup do
     @issue = issues(:bug_report)
+    @todo = todo_items(:first_todo)
   end
 
   test "should create todo" do
     assert_difference('TodoItem.count') do
-      post :create, :issue_id => @issue.id, :todo_item => {:content => "to change the world"}
+      post :create, :issue_id => @issue.id, :todo_item => { :content => @todo.content }
     end
 
     assert assigns(:issue)
@@ -16,19 +17,32 @@ class TodoItemsControllerTest < ActionController::TestCase
   end
 
   test "should mark as done" do
-    get :do_it, :id => todo_items(:first_todo).id
+    xhr :get, :do_it, :id => @todo.id
     assert_response :success
-
     assert_equal :done, assigns(:todo_item).current_state.name
   end
 
   test "should mark as new" do
-    todo = todo_items(:first_todo)
-    todo.do_it! if todo.new?
+    @todo.do_it! if @todo.new?
 
-    get :undo, :id => todo.id
+    xhr :get, :undo, :id => @todo.id
     assert_response :success
-
     assert_equal :new, assigns(:todo_item).current_state.name
+  end
+
+  test "should destroy todo via xhr" do
+    assert_difference('TodoItem.count', -1) do
+      xhr :delete, :destroy, :id => @todo.id
+    end
+    assert_response :success
+  end
+
+  test "should destroy todo normally" do
+    assert_difference('TodoItem.count', -1) do
+      delete :destroy, :id => @todo.id
+    end
+
+    assert assigns(:todo_item)
+    assert_redirected_to [assigns(:todo_item).issue.project, assigns(:todo_item).issue]
   end
 end

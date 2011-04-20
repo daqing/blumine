7a37325e6239d04ff04757a372f6b81c8defa6bc
daqing <daqing1986@gmail.com>
2011-04-20 20:52:31 +0800

    modify UI

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  $("div.row, div.section").live('mouseenter', function() { $(this).css('backgroundColor', '#FFC'); })
+  $(".bg_highlight").live('mouseenter', function() { $(this).css('backgroundColor', '#FFC'); })
     .live('mouseleave', function() { $(this).css('backgroundColor', '#FFF'); });
 });

d1094a28bb6acd06e154e53b23aaba9d598a48c2
daqing <daqing1986@gmail.com>
2011-05-03 19:25:51 +0800

    load rvm in cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,15 @@
 ssh_options[:username] = 'ubuntu'
 ssh_options[:keys] = [File.join(ENV['HOME'], '.ec2', 'gsg-keypair.pem')]
 
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# # Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+# set :rvm_ruby_string, '1.9.2'
+set :rvm_type, :user  # Don't use system-wide RVM
+
 set :application, "blumine"
 set :repository,  "git://github.com/daqing/blumine.git"
 

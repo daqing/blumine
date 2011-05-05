2a9776c96e42b63f4135d4eaf0f548f666eb9caa
daqing <daqing1986@gmail.com>
2011-05-05 15:19:56 +0800

    remove facebox_render plugin

--- vendor/plugins/facebox_render/lib/facebox_render_helper.rb
+++
@@ -1,7 +0,0 @@
-module FaceboxRenderHelper
-
-  def facebox_link_to(name, options = {}, html_options = nil)  
-    link_to_function(name, "jQuery.facebox(function(){ #{remote_function(options)} })", html_options || options.delete(:html))
-  end
-
-end

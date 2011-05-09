c17a98503a9f50daa7b862c24593f967a6c631ca
daqing <daqing1986@gmail.com>
2011-05-09 23:15:47 +0800

    only show people who're working on something

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   def index
     if current_user 
       @title = t(:dashboard)
+      @users_working_on = {}
+      Issue.only_working_on.each do |issue|
+        user = issue.assigned_user
+        if @users_working_on[user]
+          @users_working_on[user] << issue
+        else
+          @users_working_on[user] = [issue]
+        end
+      end
       render 'dashboard'
     else
       @title = t(:home)

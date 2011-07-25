248c562d26d2e14c5be78a86d1989201b73c223c
daqing <daqing1986@gmail.com>
2011-07-25 11:36:55 +0800

    add attachment to replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -5,12 +5,21 @@ class RepliesController < ApplicationController
     @reply = @conversation.replies.build(params[:reply])
     @reply.user = current_user
 
+    @reply.uploads.each do |upload|
+      upload.user = current_user
+      upload.project = @project
+    end
+
     respond_to do |f|
       if @reply.save
         f.html { redirect_to [@project, @conversation] }
         f.js
       else
-        f.html { redirect_to @project, :error => 'save failed'}
+        f.html {
+          flash[:error] = t('error.save_failed')
+          redirect_to @project
+        }
+
         f.js { render :text => :error, :status => 500 }
       end
     end

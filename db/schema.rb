14254c3d5d2d55deaa9f30a444174fd8ba559d67
daqing <daqing1986@gmail.com>
2011-10-10 11:54:59 +0800

    add rvmrc

--- db/schema.rb
+++ db/schema.rb
@@ -74,13 +74,6 @@ ActiveRecord::Schema.define(:version => 20110605071315) do
     t.datetime "updated_at"
   end
 
-  create_table "project_users", :force => true do |t|
-    t.integer  "project_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "projects", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
